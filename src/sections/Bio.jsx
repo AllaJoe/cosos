9dd4612173fbc75eee2e81d617a55b2b153f1685
Alla Joe <freddorlost45@gmail.com>
2023-05-17 13:25:27 -0300

    nada pruebita

--- src/sections/Bio.jsx
+++ src/sections/Bio.jsx
@@ -33,14 +33,14 @@ const Bio = () => {
             <Modalcito  
             img1="https://i.ibb.co/tCvjB6m/00007-322222213778-1.png"
             img="https://i.ibb.co/m9XxwSV/00011-556346363-gigapixel-standard-scale-4-00x.jpg"
-            h3="TITULon CARD 1"
+            h3="TITULONCITO CARD 1"
             p="Textito para la card que va abajo de la imagen y el titulo que se rellena con lo que sea y asi lo estoy hacieno"
             />
 
             <Modalcito  
             img1="https://i.ibb.co/fQmPYdt/00056-5555636379.png"
             img="https://i.ibb.co/N6Dq3Cv/00005-3354756848346-gigapixel-standard-scale-4-00x-gigapixel-standard-scale-1-00x.jpg" 
-            h3="TITULITO CARD 2"
+            h3="TITULONCITO CARD 2"
             p="Textito para la card que va abajo de la imagen y el titulo que se rellena con lo que sea y asi lo estoy hacieno"
             />
             
@@ -48,14 +48,14 @@ const Bio = () => {
             <Modalcito  
             img1="https://i.ibb.co/8cHcgxY/00055-5555636378.png"
             img="https://i.ibb.co/Ld3BcR5/00019-88805685333-gigapixel-standard-scale-6-00x.jpg" redit="https://i.ibb.co/Ld3BcR5/00019-88805685333-gigapixel-standard-scale-6-00x.jpg"
-            h3="TITULITO CARD 3"
+            h3="TITULONCITO CARD 3"
             p="Textito para la card que va abajo de la imagen y el titulo que se rellena con lo que sea y asi lo estoy hacieno"
             />
             
             <Modalcito  
             img1="https://i.ibb.co/RzXQk39/00054-5555636377.png"
             img="https://i.ibb.co/ys1RBFs/Pantallazo-11-05-2023-01-36-09.png" 
-            h3="TITULITO CARD 2"
+            h3="TITULONCITO CARD 4"
             p="Textito para la card que va abajo de la imagen y el titulo que se rellena con lo que sea y asi lo estoy hacieno"            
             />
 
